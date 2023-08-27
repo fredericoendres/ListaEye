16a5b8b17b23954b2f58a3e19b71a3f217200ab5
fredericoendres <116963749+fredericoendres@users.noreply.github.com>
2023-08-27 20:09:45 -0300

    Implementado o retrofit dentro da MainActivity, populando o texto com o primeiroNome. Utilizado só o metodo de get por enquanto.
    Erro presente: Ao salvar uma pessoa com o mesmo ID passado pelo metodo de editar, está criando um registro novo.

---
+++ app/src/main/java/devandroid/frederico/listaeye/network/Retrofit.java
@@ -0,0 +1,29 @@
+package devandroid.frederico.listaeye.network;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Retrofit {
+
+    private static String baseurl = "http://demo8004616.mockable.io/";
+
+    static Gson gson = new GsonBuilder()
+            .setLenient()
+            .create();
+
+    private static retrofit2.Retrofit retrofit;
+
+    public static retrofit2.Retrofit getRetrofit() {
+
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(baseurl)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
